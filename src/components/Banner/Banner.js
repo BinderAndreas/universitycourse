6750b9b3723bb2ff61a3d381fb21a78a2949268c
BinderAndreas <andreas.binder93@gmail.com>
2023-02-12 16:30:10 +0100

    first changes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,7 +5,7 @@ import businessLady from "../../assets/img/businesslady.png"
 function Banner() {
   return (
     <div className='banner'>
-        <p>Bestehe deine Betriebswirtschaftsprüfung mit unseren umfassenden Lernnotizen - Hole sie dir jetzt!</p>
+        <p>Bestehe deine Betriebswirtschaftsprüfung mit unseren umfassenden Lernnotizen - Hol sie dir jetzt!</p>
         <img src={businessLady} alt="business lady" className='businesslady'/>
     </div>
   )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./header.css"
-import { FcGraduationCap } from "react-icons/fc";
 import {Link,useNavigate} from "react-router-dom"
 import logoBlack from "../../assets/img/logo-black - bearbeitet.png"
 
@@ -10,7 +9,6 @@ export default function Header() {
   return (
     <div className='header-container'>
       <img src={logoBlack} alt="Logo" className='logo-black'/>
-        
         <a href="/">Kursübersicht</a>
         <a href="/">Mein Kursbereich</a>
         <a href="/">About us</a>
